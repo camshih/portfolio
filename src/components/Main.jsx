21141e8b63879ea0e7a07f2ab4432d7058107fbc
Cameron Shih <camshih@gmail.com>
2022-07-10 02:48:07 -0400

    main page idea

---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+const Main = () => {
+    return (
+        <div className='w-full h-screen bg-slate-200 flex flex-col '>
+            <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
+                <div>
+                    <h2>Cameron Shih</h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, deleniti.</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default Main
