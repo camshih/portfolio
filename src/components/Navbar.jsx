ec04babf3372098a58f64b3cad573cd63e37f59a
Cameron Shih <camshih@gmail.com>
2022-07-10 02:47:53 -0400

    added responsive and href

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,29 +4,29 @@ import {MenuIcon, XIcon} from '@heroicons/react/outline'
 const Navbar = () => {
 
     const [nav, setNav] = useState(false)
-    const handleClick =() => setNav(!nav)
+    const handleClick = () => setNav(!nav)
     return (
         <div className='w-full h-20 bg-slate-200 fixed drop-shadow-lg'>
             <div className='px-8 flex justify-between items-center w-full h-full'>
-            
+
                 <div className='flex'>
-                    <h1 className='text-3xl font-bold sm:text-4xl'>Cameron Shih</h1>
+                    <h1 className='text-3xl font-bold sm:text-4xl '><ul><li><a href=''>Cameron Shih</a></li></ul></h1>
                 </div>
 
-                <ul className='hidden md:flex pr-20'>
-                    <li>Projects</li>
-                    <li>Resume</li>
+                <ul className='hidden md:flex pr-8'>
+                    <li><a href='#'>Projects</a></li>
+                    <li><a href='#'>Resume</a></li>
                 </ul>
 
                 {/* Responsive Menu Icon */}
                 <div className='md:hidden' onClick={handleClick}>
-                    {!nav ? <MenuIcon className='w-6'/> : <XIcon className='w-6'/>}
+                    {!nav ? <MenuIcon className='w-4'/> : <XIcon className='w-4'/>}
                 </div>
 
             </div>
 
-            {/* Responsive Menu */}
-            <ul className={!nav ? 'hidden' : 'absolute bg-slate-200 w-full px-12'}>
+            {/* Mobile Menu */}
+            <ul className={!nav ? 'hidden' : 'absolute bg-slate-200 w-full px-8'}>
                 <li>Projects</li>
                 <li>Resume</li>
             </ul>
